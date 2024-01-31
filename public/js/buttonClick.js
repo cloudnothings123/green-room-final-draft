795f9dc3c34f9624be83fd72c778d48e530fda8d
Dylan Baldi <cloudnothings@gmail.com>
2024-01-31 17:28:57 -0500

    adding

---
+++ public/js/buttonClick.js
@@ -0,0 +1,19 @@
+document.querySelector(".speedy-button").addEventListener("click", () => {
+
+    let speedyTable = document.querySelector(".speedy-table")
+    if (speedyTable.style.display === "none") {
+        speedyTable.style.display = "block";
+    } else {
+        speedyTable.style.display = "none";
+    }
+  });
+
+  document.querySelector(".john").addEventListener("click", () => {
+
+    let johnTable = document.querySelector(".john-table")
+    if (johnTable.style.display === "none") {
+        johnTable.style.display = "block";
+    } else {
+        johnTable.style.display = "none";
+    }
+  });
